70303ac4c37689a742e4671b409db20bb9ae64be
pelukissenja <99958892+pelukissenja@users.noreply.github.com>
2023-01-12 22:46:51 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,196 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="theme-color" content="#422f80" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./css/index.css">
+  <title>portofolio</title>
+</head>
+<body>
+	<header>
+	  <nav>
+	    <div class="brand">
+	      <h4>
+	        Hasan
+	      </h4>
+	    </div>
+	    <div class="burgermenu" id="burgermenu">
+	      <div class="burger" id="burger">
+	        <ion-icon name="menu"></ion-icon>
+	      </div>
+  	    <ul class="menulist" id="menulist">
+  	      <li><a href="?about">About</a></li>
+  	      <li><a href="?project">Project</a></li>
+  	      <li><a href="?skill">Skill</a></a></li>
+  	      <li><a href="?gallery">Gallery</a></a></li>
+  	      <li><a href="?testi">Testimony</a></a></li>
+  	      <li><a href="?contact">Contact</a></a></li>
+  	    </ul>
+	    </div>
+	  </nav>
+	</header>
+	   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#422f80" fill-opacity="1" d="M0,0L18.5,53.3C36.9,107,74,213,111,240C147.7,267,185,213,222,208C258.5,203,295,245,332,256C369.2,267,406,245,443,224C480,203,517,181,554,170.7C590.8,160,628,160,665,165.3C701.5,171,738,181,775,208C812.3,235,849,277,886,266.7C923.1,256,960,192,997,144C1033.8,96,1071,64,1108,69.3C1144.6,75,1182,117,1218,144C1255.4,171,1292,181,1329,181.3C1366.2,181,1403,171,1422,165.3L1440,160L1440,0L1421.5,0C1403.1,0,1366,0,1329,0C1292.3,0,1255,0,1218,0C1181.5,0,1145,0,1108,0C1070.8,0,1034,0,997,0C960,0,923,0,886,0C849.2,0,812,0,775,0C738.5,0,702,0,665,0C627.7,0,591,0,554,0C516.9,0,480,0,443,0C406.2,0,369,0,332,0C295.4,0,258,0,222,0C184.6,0,148,0,111,0C73.8,0,37,0,18,0L0,0Z"></path>
+	   </svg>
+	<main>
+	    <figure class="avatar">
+	      <img src="./images/author.jpg" alt="author" class="rounded">
+	    </figure>
+	  <?php if(isset($_REQUEST['skill'])){?>
+	  <section id="skill">
+	      <div class="title">
+	        <h4>My</h4>
+	        <h4>Skill</h4>
+	      </div>
+	      <div class="skill-content">
+  	      <div class="card">
+  	        <h4>First Skill</h4>
+  	        <div class="info">
+  	          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia animi quo voluptate nostrum eum rem dolore, obcaecati? Qui fuga veritatis rem aliquam, ipsam temporibus. Ipsum dicta porro sunt enim fuga. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur voluptatibus nemo optio ea saepe tempora voluptates nihil ab laboriosam aperiam doloremque, fugiat recusandae asperiores soluta aut, dignissimos neque beatae inventore.
+  	          </div>
+  	      </div>
+  	      <div class="card">
+  	        <h4>First Skill</h4>
+  	        <div class="info">
+  	          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia animi quo voluptate nostrum eum rem dolore, obcaecati? Qui fuga veritatis rem aliquam, ipsam temporibus. Ipsum dicta porro sunt enim fuga. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur voluptatibus nemo optio ea saepe tempora voluptates nihil ab laboriosam aperiam doloremque, fugiat recusandae asperiores soluta aut, dignissimos neque beatae inventore.
+  	          </div>
+  	      </div>
+  	      <div class="card">
+  	        <h4>First Skill</h4>
+  	        <div class="info">
+  	          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia animi quo voluptate nostrum eum rem dolore, obcaecati? Qui fuga veritatis rem aliquam, ipsam temporibus. Ipsum dicta porro sunt enim fuga. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur voluptatibus nemo optio ea saepe tempora voluptates nihil ab laboriosam aperiam doloremque, fugiat recusandae asperiores soluta aut, dignissimos neque beatae inventore.
+  	          </div>
+  	      </div>
+  	      <div class="card">
+  	        <h4>First Skill</h4>
+  	        <div class="info">
+  	          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia animi quo voluptate nostrum eum rem dolore, obcaecati? Qui fuga veritatis rem aliquam, ipsam temporibus. Ipsum dicta porro sunt enim fuga. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur voluptatibus nemo optio ea saepe tempora voluptates nihil ab laboriosam aperiam doloremque, fugiat recusandae asperiores soluta aut, dignissimos neque beatae inventore.
+  	          </div>
+  	      </div>
+  	      <div class="card">
+  	        <h4>First Skill</h4>
+  	        <div class="info">
+  	          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia animi quo voluptate nostrum eum rem dolore, obcaecati? Qui fuga veritatis rem aliquam, ipsam temporibus. Ipsum dicta porro sunt enim fuga. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur voluptatibus nemo optio ea saepe tempora voluptates nihil ab laboriosam aperiam doloremque, fugiat recusandae asperiores soluta aut, dignissimos neque beatae inventore.
+  	          </div>
+  	      </div>
+  	      <div class="card">
+  	        <h4>First Skill</h4>
+  	        <div class="info">
+  	          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia animi quo voluptate nostrum eum rem dolore, obcaecati? Qui fuga veritatis rem aliquam, ipsam temporibus. Ipsum dicta porro sunt enim fuga. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur voluptatibus nemo optio ea saepe tempora voluptates nihil ab laboriosam aperiam doloremque, fugiat recusandae asperiores soluta aut, dignissimos neque beatae inventore.
+  	          </div>
+  	      </div>
+	      </div>
+	  </section>
+	  <?php }elseif(isset($_REQUEST['gallery'])){?>
+	  <section id="gallery">
+	    <h1>Gallery</h1>
+	  </section>
+	  <?php }elseif(isset($_REQUEST['project'])){?>
+	  <section id="project">
+	    <div class="title">
+	      <h4>My</h4>
+	      <h4>Projects</h4>
+	    </div>
+	    <div class="project-content">
+	      <div class="card project-card">
+	      <figure>
+	        <img src="./images/author.jpg" alt="project1" class="rounded">
+	      </figure>
+	      <div class="detail">
+	        <div class="top-detail">
+	          <h4>BACKEND SIAKAD STIE PPI</h4>
+	          <h5><?=date('Y')-3?> - <?=date('Y')-2?> </h5>
+	        </div>
+	        <p>
+	          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti suscipit nisi error culpa repudiandae, sint earum. Asperiores, ipsa atque, doloremque eius in animi quas, quis ut tenetur vero ad, enim.
+	        </p>
+	      </div>
+	    </div>
+	      <div class="card project-card">
+	      <figure>
+	        <img src="./images/author.jpg" alt="project1" class="rounded">
+	      </figure>
+	      <div class="detail">
+	        <div class="top-detail">
+	          <h4>BACKEND ASSALAM TOWN HOUSE</h4>
+	          <h5><?=date('Y')-4?> - <?=date('Y')-2?> </h5>
+	        </div>
+	        <p>
+	          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti suscipit nisi error culpa repudiandae, sint earum. Asperiores, ipsa atque, doloremque eius in animi quas, quis ut tenetur vero ad, enim.
+	        </p>
+	      </div>
+	    </div>
+	      <div class="card project-card">
+	      <figure>
+	        <img src="./images/author.jpg" alt="project1" class="rounded">
+	      </figure>
+	      <div class="detail">
+	        <div class="top-detail">
+	          <h4>DATA ANALYST SIPESEK YPI NURUL HUDA</h4>
+	          <h5><?=date('Y')-5?> - <?=date('Y')-2?> </h5>
+	        </div>
+	        <p>
+	          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti suscipit nisi error culpa repudiandae, sint earum. Asperiores, ipsa atque, doloremque eius in animi quas, quis ut tenetur vero ad, enim.
+	        </p>
+	      </div>
+	    </div>
+	    </div>
+	  </section>
+	  <?php }elseif(isset($_REQUEST['testi'])){?>
+	  <section id="testi">
+	    <h1>Testimoni</h1>
+	  </section>
+	  <?php }elseif(isset($_REQUEST['contact'])){?>
+	  <section id="contact">
+	    <h1>Cobtact</h1>
+	  </section>
+	  <?php }else{?>
+	  <section id="about">
+
+	    <div class="aboutme">
+	      <div class="title">
+	      <h4>about</h4>
+	      <h4>me</h4>
+	    </div>
+	      <p>
+	      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt quam dolorem aut odio dolores, quasi impedit nostrum perspiciatis ullam suscipit beatae consequatur ipsam, quidem fugit culpa odit! Provident beatae, alias. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus eos quo neque quisquam harum, cum amet necessitatibus perspiciatis vero enim, explicabo laborum nisi voluptas illum consectetur unde minima culpa porro.
+	    </p>
+	    </div>
+	  </section>
+	  <?php } ?>
+	</main>
+	<footer>
+	    <div class="sosmed">
+	      <div class="fb">
+	        <ion-icon name="logo-facebook"></ion-icon>
+	      </div>
+	      <div class="wa">
+	        <a href="wa.me/6281315269591">
+	        <ion-icon name="logo-whatsapp"></ion-icon>
+	        </a>
+	      </div>
+	      <div class="ig">
+	        <a href="//facebook.com/temenngoding">
+	        <ion-icon name="logo-instagram"></ion-icon>
+	        </a>
+	      </div>
+	      <div class="yt">
+	        <a href="//youtube.com/">
+	        <ion-icon name="logo-youtube"></ion-icon>
+	        </a>
+	      </div>
+	    </div>
+	    &copy; temenngoding Allright reserved
+	</footer>
+	<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+	<script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script>
+  <script src="./js/index.js"></script>
+  <script>
+    let btn = $('#burger');
+    $(btn).on('click', function(){
+      $('#menulist').toggle('slow')
+    })
+  </script>
+  </script>
+</body>
+</html>
