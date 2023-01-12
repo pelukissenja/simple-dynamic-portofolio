0f1f50b716ce096789c0e854f9c699d926b852b2
pelukissenja <99958892+pelukissenja@users.noreply.github.com>
2023-01-12 22:48:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="theme-color" content="#422f80" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="./css/index.css">
+  <link rel="stylesheet" href="./index.css">
   <title>portofolio</title>
 </head>
 <body>
